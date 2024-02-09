a8e0fc03b1f67eb8488c2afeafa38837150350bc
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-02-09 19:16:22 +0530

    data ingestion added

---
+++ main.py
@@ -0,0 +1,17 @@
+import os, sys
+
+from xray.exception import XRayException
+from xray.pipeline.train_pipeline import TrainingPipeline
+
+def start_training():
+    try:
+        
+        train_pipeline = TrainingPipeline()
+        
+        train_pipeline.run_pipeline()
+    except Exception as e:
+        raise XRayException(e, sys)
+    
+    
+if __name__ == '__main__':
+    start_training()
